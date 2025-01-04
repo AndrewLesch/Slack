4fcb3c41910de59580c797fc8bf7940cba133e80
AndrewLesch <Andreykabreamz@gmail.com>
2025-01-04 18:46:25 +0300

    Logout fix

---
+++ src/app/logout-wrapper.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useLogout } from "@/context/logout-context";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const LogoutWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoggingOut, setIsLoggingOut } = useLogout();
+  const { signOut } = useAuthActions();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoggingOut) {
+      signOut()
+        .then(() => {
+          router.replace("/auth"); 
+        })
+        .catch((error) => {
+          console.error("Error during sign out:", error);
+        })
+        .finally(() => {
+          setIsLoggingOut(false);
+        });
+    }
+  }, [isLoggingOut, signOut, router, setIsLoggingOut]); 
+
+  if (isLoggingOut) {
+    return <AuthScreen />;
+  }
+
+  return <>{children}</>; 
+};
+
+export default LogoutWrapper;
