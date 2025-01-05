fdc3e131857f13c8c3f366087d24021f5b842196
AndrewLesch <Andreykabreamz@gmail.com>
2025-01-05 21:31:41 +0300

    Test

--- src/components/channel-hero.tsx
+++ src/components/channel-hero.tsx
@@ -12,6 +12,8 @@ export const ChannelHero = ({name, creationTime}: ChannelHeroProps) => {
 
 
   
+
+
   return (
     <div className="mt-[88px] mx-5 mb-4">
       <p className="text-2xl font-bold flex items-center mb-2">
